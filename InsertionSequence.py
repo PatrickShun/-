3dbebf0645d23130cd17c0b345c55b6d86ed72b3
inkl_Leuchtmittel <torrey@126.com>
2022-06-09 13:42:07 +0800

    main

--- InsertionSequence.py
+++ InsertionSequence.py
@@ -2,6 +2,20 @@
 # 首先最左端的值已完成排序，然后将第二个开始的数值执行对比，如果比第二个数值比第一个数值小，则交换位置，移动后再次对比前面的值，一直重复；
 
 l1 = [2,3,6,4,5,8,9,1,7]
+count = 0
+for i in range(1,9):
+    # print(l1[i])
+    for k in range(len(l1)):
+        if l1[i] <= l1[k]:
+            print("换")
+            l1[i],l1[k] = l1[k],l1[i]
+            print(l1,"\n")
+            count += 1
+        else:
+            print("PASS")
+            print(l1,"\n")
 
-for i in range(len(l1)):
-    print(l1[i],l1[i+1])
+    print("=" * 30)
+
+print(l1)
+print(count)
