e1b489fa03dbf8c271641e320d563a2bff2d777d
inkl_Leuchtmittel <torrey@126.com>
2022-06-10 20:43:08 +0800

    main

--- SelectiveSort.py
+++ SelectiveSort.py
@@ -1,21 +1,13 @@
 # 选择性排序，线性搜索序列，找到最小值，并移动到第一位；
 
 
-l1 = [2,3,6,4,5,8,9,1,7]
+l1 = [2,1,6,4,5,8,9,5,7]
 
-# print(len(l1))
 for i in range(len(l1)):
-    key1 = l1[i]
-    for j in range(len(l1)):
-        key2 = l1[j]
-        if key1 >= key2:
-            print("PASS")
-        else:
-            print("移动")
-            l1[i],l1[j] = l1[j],l1[i]
-    print("="*30)
+    x = min(l1[i::]) # 获取列表的最小值
+    indX = l1.index(x) # 获取最小值在列表的位置
+    print(l1[indX])
 
-
-print(l1)
+# print(l1)
 
 
