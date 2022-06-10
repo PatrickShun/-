624530465d554df69f810380aa15c20bbce27d6b
inkl_Leuchtmittel <torrey@126.com>
2022-06-10 17:43:06 +0800

    man

--- SmokingAlgorithm.py
+++ SmokingAlgorithm.py
@@ -2,19 +2,20 @@
 # 冒泡序列
 # 在一个序列中对比最后两个的数值，对比后把小的移动到前面，否则不动，运行多次后可把序列按顺序排好
 
-l1 = [8,9,5,6,4,3,1,2,7]
+l1 = [8,9,5,6,4,3,1,2,7,0,1,2]
 
 def MySmoke():
-    for n in range(8): # 循环列表的数值，l1[n]为列表数值；
+    for n in range(len(l1)-1): # 循环列表的数值，l1[n]为列表数值；
         p = n+1
         x = l1[n]
         y = l1[p]
-        if x < y:
+        if x >= y:
             print("换")
             l1[n],l1[p] = l1[p],l1[n]
         else:
             print("PASS")
     print(l1)
+
 for i in range(len(l1)):
     MySmoke()
 
